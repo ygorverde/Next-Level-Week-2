019e50dd5974f97c7b476c36195aab5912a07298
ygorverde <ygorverde572@gmail.com>
2020-08-08 00:04:14 -0300

    Setup Inicial Mobile

--- mobile/src/components/PageHeader/index.tsx
+++ mobile/src/components/PageHeader/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { ReactNode } from 'react';
 import { View, Image, Text } from 'react-native';
 import { BorderlessButton } from 'react-native-gesture-handler';
 
@@ -8,12 +8,12 @@ import logoImg from '../../assets/images/logo.png';
 import { useNavigation } from '@react-navigation/native';
 
 import styles from './styles';
-
 interface PageHeaderProps {
     title: string;
+    headerRight?: ReactNode;     // Possibilita receber um componente como propriedade (ReactNode);
 }
 
-const PageHeader: React.FC<PageHeaderProps> = ({ title }) => {
+const PageHeader: React.FC<PageHeaderProps> = ({ title, children, headerRight }) => {
     const navigation = useNavigation();
     function handleGoBack() {
         navigation.navigate('Landing');
@@ -29,7 +29,13 @@ const PageHeader: React.FC<PageHeaderProps> = ({ title }) => {
             <Image source={logoImg} resizeMode="contain" />
         </View>
 
+        <View style={styles.header}>
+
         <Text style={styles.title}>{title}</Text>
+        {headerRight}
+        
+        </View>
+        {children}
     </View>
     );
 }
